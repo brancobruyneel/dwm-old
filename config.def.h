56ef069b02fa143add21fc33691d9e4e576bbd3c
Branco <branco.bruyneel@gmail.com>
2020-08-15 22:15:46 +0200

    added fonts & changed tags

--- config.def.h
+++ config.def.h
@@ -6,7 +6,12 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { 
+    "Hack:size=10",
+    "Font Awesome 5 Brands:size=10",
+    "Font Awesome 5 Free Solid:size=10",
+    "Font Awesome 5 Free Regular:size=10",
+};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -21,7 +26,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
